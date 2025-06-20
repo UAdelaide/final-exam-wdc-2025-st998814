3e65a5eca42036d26a7216525f38dfbcdb76b7fd
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 02:46:26 +0000

    [automatic commit] 2025-06-20 02:46:26 UTC 49.178.137.56

--- part1/app.js
+++ part1/app.js
@@ -65,12 +65,31 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open',async(req, res)=>{
     try{
+        const [rows] = await pool.query(`
+      SELECT
+        WalkRequests.request_id,
+        Dogs.name AS dog_name,
+        WalkRequests.requested_time,
+        WalkRequests.duration_minutes,
+        WalkRequests.location,
+        Users.username AS owner_username
+      FROM WalkRequests
+      JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+      JOIN Users ON Dogs.owner_id = Users.user_id
+      WHERE WalkRequests.status = 'open'
+    `);
+    res.json(rows);
+     } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 
 
 
-        
+
     }
 
 
