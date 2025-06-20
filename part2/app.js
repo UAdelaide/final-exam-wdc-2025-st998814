32ddaffdcd266d2df35650500c03f828d323a4fd
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 09:30:00 +0000

    [automatic commit] 2025-06-20 09:29:59 UTC 49.178.137.56

--- part2/app.js
+++ part2/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = require('express-session');
+
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+const session = require('express-session');
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
