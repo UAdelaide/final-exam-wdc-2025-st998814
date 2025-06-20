c14ab5a0983bbc717264bc11d10d18e561b532be
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 09:08:46 +0000

    [automatic commit] 2025-06-20 09:08:46 UTC 49.178.137.56

--- part2/app.js
+++ part2/app.js
@@ -15,6 +15,11 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use
+app.use(session({
+
+
+
+    
+}))
 // Export the app instead of listening here
 module.exports = app;
