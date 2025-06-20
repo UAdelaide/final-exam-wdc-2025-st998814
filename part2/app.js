6f2651ad2900b442fc3ff14e7ecc8cc20f1f8e0f
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 09:16:38 +0000

    [automatic commit] 2025-06-20 09:16:38 UTC 49.178.137.56

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,13 @@ app.use(session({
 
 app.post('/login',async(req,res)=>{
     const { username, password } = req.body;
+    try {
+        const [rows] = await pool.query(
+            `SELECT * FROM Users WHERE username = ? AND password_hash = ?`,
+            [username, password] 
+        );
+
+
 
 
 
