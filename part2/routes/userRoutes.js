a2f6d52855039203ead839f88e411967faa7bedf
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 11:30:59 +0000

    [automatic commit] 2025-06-20 11:30:59 UTC 49.178.137.56

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const [rows] = await db.query(`
+    const [rows] = await req.db.query(`
       SELECT user_id, username, role
       FROM Users
       WHERE username = ? AND password_hash = ?
