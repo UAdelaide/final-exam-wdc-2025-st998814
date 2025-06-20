6c0c113784fb8e619682477b66554124785acb4e
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 09:21:47 +0000

    [automatic commit] 2025-06-20 09:21:47 UTC 49.178.137.56

--- part2/app.js
+++ part2/app.js
@@ -49,15 +49,6 @@ app.post('/login',async(req,res)=>{
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
-
-
-
-
-
-
-
-
-
 });
 // Export the app instead of listening here
 module.exports = app;
