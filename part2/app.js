7c3d418fcb1657beb20c9798e1f9c70aa8f757bf
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 09:18:43 +0000

    [automatic commit] 2025-06-20 09:18:43 UTC 49.178.137.56

--- part2/app.js
+++ part2/app.js
@@ -36,7 +36,13 @@ app.post('/login',async(req,res)=>{
             };
             if (rows[0].role === 'owner') {
                 res.redirect('/owner-dashboard');
-            } else if
+            } else if (rows[0].role === 'walker'){
+                res.redirect('/owner-dashboard');
+
+
+
+
+            }
 
 
 
