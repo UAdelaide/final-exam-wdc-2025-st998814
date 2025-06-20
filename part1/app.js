60aeab9e82ea7d8b07f0e113f7261eb6055fc30f
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 11:03:34 +0000

    [automatic commit] 2025-06-20 11:03:34 UTC 49.178.137.56

--- part1/app.js
+++ part1/app.js
@@ -27,46 +27,46 @@ const dbConfig = {
 
 let pool;
 // init db everytime
-async function initDb() {
-  pool = await mysql.createPool(dbConfig);
-    await pool.query(`INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
-    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+// async function initDb() {
+//   pool = await mysql.createPool(dbConfig);
+//     await pool.query(`INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
+//     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 
-    ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+//     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 
-    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+//     ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 
-    ('devdog', 'devdog@example.com', 'hashed444', 'owner'),
+//     ('devdog', 'devdog@example.com', 'hashed444', 'owner'),
 
-    ('dogdev', 'dogdev@example.com', 'hashed7414', 'walker');
-  `);
+//     ('dogdev', 'dogdev@example.com', 'hashed7414', 'walker');
+//   `);
 
-  await pool.query(`INSERT IGNORE INTO Dogs (owner_id, name, size) VALUES
-    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+//   await pool.query(`INSERT IGNORE INTO Dogs (owner_id, name, size) VALUES
+//     ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 
-    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+//     ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 
-    ((SELECT user_id FROM Users WHERE username = 'devdog'), 'Rocky', 'large'),
+//     ((SELECT user_id FROM Users WHERE username = 'devdog'), 'Rocky', 'large'),
 
-    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Mcgrady', 'small'),
+//     ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Mcgrady', 'small'),
 
-    ((SELECT user_id FROM Users WHERE username = 'dogdev'), 'Tim', 'medium');
+//     ((SELECT user_id FROM Users WHERE username = 'dogdev'), 'Tim', 'medium');
 
-  `);
+//   `);
 
-  await pool.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-    ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+//   await pool.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+//     ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 
-    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+//     ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 
-    ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'Central Park', 'open'),
+//     ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'Central Park', 'open'),
 
-    ((SELECT dog_id FROM Dogs WHERE name = 'Mcgrady'), '2025-06-12 11:00:00', 30, 'Greenwood Park', 'open'),
+//     ((SELECT dog_id FROM Dogs WHERE name = 'Mcgrady'), '2025-06-12 11:00:00', 30, 'Greenwood Park', 'open'),
 
-    ((SELECT dog_id FROM Dogs WHERE name = 'Tim'), '2025-06-13 14:00:00', 45, 'Seaside Blvd', 'open');
+//     ((SELECT dog_id FROM Dogs WHERE name = 'Tim'), '2025-06-13 14:00:00', 45, 'Seaside Blvd', 'open');
 
-  `);
-}
+//   `);
+// }
 
 
 
