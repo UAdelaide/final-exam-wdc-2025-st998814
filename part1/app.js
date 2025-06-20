0921ad7d74b5638b8a14ba8bdef5e304c8807e65
st998814 <116810151+st998814@users.noreply.github.com>
2025-06-20 03:16:39 +0000

    [automatic commit] 2025-06-20 03:16:39 UTC 49.178.137.56

--- part1/app.js
+++ part1/app.js
@@ -14,7 +14,7 @@ let pool;
 async function initDb() {
   pool = await mysql.createPool(dbConfig);
 
-  // 初始化資料 — 插入 Users, Dogs, WalkRequests
+  
   await pool.query(`INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
